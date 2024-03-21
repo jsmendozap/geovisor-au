aaa2837712e50edd803e6283ac668fb2ff6190c3
Juan Mendoza <jsmendozap@unal.edu.co>
2024-03-21 12:17:18 -0500

    Changes in the title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import trees from "../media/trees.svg";
 
 const Header = () => {
   return (
-    <div className="bg-[#162945] py-2 px-4">
-      <span className="font-bold text-xl text-gray-50">
+    <div className="flex flex-row items-center bg-[#162945] py-2 px-4">
+      <img src={trees} width="35px" alt="trees" className="mr-2" />
+      <span
+        className="font-semibold text-xl text-gray-50"
+        style={{ fontFamily: "Inclusive Sans", fontSize: "22px" }}
+      >
         Arbolado urbano de Ibagué
       </span>
     </div>
